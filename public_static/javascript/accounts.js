3d878082072675951244ac3ac61bf948ea2d6da8
arvindkalra <kalarvind97@gmail.com>
2018-02-14 00:59:00 +0530

    HTML Connected to Express Server

--- public_static/javascript/accounts.js
+++ public_static/javascript/accounts.js
@@ -1,10 +1,31 @@
 
 $(document).ready(function () {
+  var curraccount;
+  var selectedAccount;
   $.get('/getAccounts', function (response) {
-
     for(let i = 0; i < response.length; i++){
-      let curraccount = response[i];
+      curraccount = response[i];
       $('#options').append("<option value='"+curraccount+"'>"+curraccount+"</option>");
     }
   })
+
+  $('#submit').click(function () {
+    selectedAccount = $('#options').val();
+    console.log(selectedAccount);
+    $.post('/getBalance', {account : selectedAccount}, function (response) {
+      $('.select').removeClass("active");
+      $('.send').addClass("active");
+      $('#account').text(selectedAccount);
+      $('#balance').text(response);
+    })
+  })
+
+  $('#send').click(function () {
+    $('#status').text("Sending...");
+    let amount = $('#amount').val();
+    let receiver = $('#receiver').val();
+    $.post('/sendCoin', {amount : amount, sender : selectedAccount, receiver : receiver}, function (response) {
+      $('#balance').text(response);
+    })
+  });
 })
