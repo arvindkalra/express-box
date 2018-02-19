30fc2893b3baf01b891f7d7a933eff4ccb26df31
arvindkalra <kalarvind97@gmail.com>
2018-02-19 18:02:50 +0530

    Minor Changes

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ app.listen(port, () => {
       // Use Mist/MetaMask's provider
         truffle_connect.web3 = new Web3(web3.currentProvider);
     } else {
-        console.warn("No web3 detected. Falling back to http://127.0.0.1:9545. You should remove this fallback when you deploy live, as it's inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask");
+        console.warn("No web3 detected. Falling back to http://127.0.0.1:8545. You should remove this fallback when you deploy live, as it's inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask");
         // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
         truffle_connect.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
     }
