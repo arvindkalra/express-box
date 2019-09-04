8ea24390d1066544aee917e224274d9cd55c666a
Pranav Singhal <pranavsinghal96@gmail.com>
2019-09-04 17:08:41 +0530

    fixed #12

--- connection/app.js
+++ connection/app.js
@@ -13,12 +13,12 @@ module.exports = {
     // Get the initial account balance so it can be displayed.
     self.web3.eth.getAccounts(function(err, accs) {
       if (err != null) {
-        alert("There was an error fetching your accounts.");
+        console.log("There was an error fetching your accounts.");
         return;
       }
 
       if (accs.length == 0) {
-        alert("Couldn't get any accounts! Make sure your Ethereum client is configured correctly.");
+        console.log("Couldn't get any accounts! Make sure your Ethereum client is configured correctly.");
         return;
       }
       self.accounts = accs;
